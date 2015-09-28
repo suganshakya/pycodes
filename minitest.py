f85a32e1f4db8633e15a4abd3639e86b66a76459
suganshakya <shakya.sugan@gmail.com>
2015-09-28 13:14:15 +0300

    Create minitest.py
    set up a mininet python API

---
+++ minitest.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+
+net = Mininet()
+h1 = net.addHost('h1')
+h2 = net.addHost('h2')
+s1 = net.addSwitch('s1')
+c0 = net.addController('c0')
+
+net.addLink(h1,s1)
+net.addLink(h2,s1)
+net.start()
+
+h2.cmd('python -m SimpleHTTPServer 80 &')
+sleep(2)
+print h1.cmd('curl', h2.IP())
+CLI(net)
+h2.cmd('kill %python')
+net.stop()
+
+
+
