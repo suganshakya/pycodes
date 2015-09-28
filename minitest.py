44e0a6b36cace3fbab54ae8c02c8ef1f87285b12
suganshakya <shakya.sugan@gmail.com>
2015-09-28 13:35:16 +0300

    Update minitest.py

--- minitest.py
+++ minitest.py
@@ -11,18 +11,11 @@ c0 = net.addController('c0')
 
 net.addLink(h1,s1)
 net.addLink(h2,s1)
-net.start()
 
+net.start()
 print("h1 ip: " + str(h1.IP()))
 print("h2 ip: " + str(h2.IP()))
-
-print(h1.ping(h2))
-
-#h2.cmd('python -m SimpleHTTPServer 80 &')
-#time.sleep(2)
-#print h1.cmd('curl', h2.IP())
-#CLI(net)
-#h2.cmd('kill %python')
+net.pingAll()
 net.stop()
 print("End")
 
