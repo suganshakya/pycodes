cdcf5c449f79f78b18d18d728905f364c8052213
suganshakya <shakya.sugan@gmail.com>
2015-09-28 13:39:51 +0300

    Update minitest.py

--- minitest.py
+++ minitest.py
@@ -2,19 +2,24 @@
 
 import time
 from mininet.net import Mininet
-# from mininet.util import createLink
+from mininet.util import createLink
+#Creating Nodes
 net = Mininet()
 h1 = net.addHost('h1')
 h2 = net.addHost('h2')
 s1 = net.addSwitch('s1')
 c0 = net.addController('c0')
-
+# Creating Links
 net.addLink(h1,s1)
 net.addLink(h2,s1)
+# Setting own IP
+h1.setIP('192.168.50.1',24)
+h2.setIP('192.168.50.2',24)
 
 net.start()
 print("h1 ip: " + str(h1.IP()))
 print("h2 ip: " + str(h2.IP()))
+net.dump()
 net.pingAll()
 net.stop()
 print("End")
