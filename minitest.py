f55ef9887ec6d623208557ea0b23b792afdbf1fe
suganshakya <shakya.sugan@gmail.com>
2015-09-28 13:52:04 +0300

    Update minitest.py

--- minitest.py
+++ minitest.py
@@ -11,13 +11,14 @@ h2 = net.addHost('h2')
 s1 = net.addSwitch('s1')
 c0 = net.addController('c0')
 
+# Setting own IP
+h1.setIP('192.168.50.1',24)
+h2.setIP('192.168.50.2',24)
+
 # Creating Links
 net.addLink(h1,s1)
 net.addLink(h2,s1)
 
-# Setting own IP
-h1.setIP('192.168.50.1',24)
-h2.setIP('192.168.50.2',24)
 
 net.start()
 print("h1 ip: " + str(h1.IP()))
